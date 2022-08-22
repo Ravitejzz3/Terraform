bcce64c7b85aca95645965b4fc952236b0a98463
Ravi Teja <ravitejzz380@gmail.com>
2022-08-22 15:15:42 +0530

    updated

---
+++ sample-ec2/ec2.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "my-ec2" {
+  ami           = "ami-00ff427d936335825"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "my-first-ec2"
+  }
+}
+
+
+output "private" {
+    value = aws_instace.b49-ec2.private_ip
+}
